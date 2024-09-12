a4e126215114ecd7df284ca7c022c0f77418c8c3
Olya <i@kukushkina86.ru>
2024-09-12 17:17:15 +0300

    add useful resources

--- src/test/java/com/example/demo/RedisInitializer.java
+++ src/test/java/com/example/demo/RedisInitializer.java
@@ -1,23 +1,22 @@
 package com.example.demo;
 
+import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class RedisInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private static final GenericContainer<?> CONTAINER = new GenericContainer<>(DockerImageName.parse("redis").withTag("4.0.14"))
-            .withExposedPorts(6379);
+    private static final RedisContainer CONTAINER = new RedisContainer(DockerImageName.parse("redis:7.4.0"));
 
     @Override
     public void initialize(ConfigurableApplicationContext context) {
         CONTAINER.start();
 
         TestPropertyValues.of(
-                "spring.data.redis.host=" + CONTAINER.getHost(),
-                "spring.data.redis.port=" + CONTAINER.getMappedPort(6379)
+                "spring.data.redis.host=" + CONTAINER.getRedisHost(),
+                "spring.data.redis.port=" + CONTAINER.getRedisPort()
         ).applyTo(context.getEnvironment());
     }
 }
